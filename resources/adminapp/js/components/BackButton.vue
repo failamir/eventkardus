202650b20c7c8ae4edc88757e581134885263a54
failamir <38488480+failamir@users.noreply.github.com>
2022-07-06 22:16:16 +0700

    reset

--- resources/adminapp/js/components/BackButton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <button type="button" class="btn btn-default" @click="$router.go(-1)">
-    <i class="material-icons">arrow_back</i>
-    {{ $t('global.back') }}
-  </button>
-</template>
--- resources/adminapp/js/pages/Layout/MobileMenu.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <ul class="nav nav-mobile-menu">
-    <top-navbar-languages />
-  </ul>
-</template>
-
-<script>
-export default {}
-</script>
